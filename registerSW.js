aac7e650435b9ce0b009f4fd9b0c557648956c79
melinoe <yugen703@gmail.com>
2025-05-24 00:54:45 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/quiz-polizia/sw.js', { scope: '/quiz-polizia/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
